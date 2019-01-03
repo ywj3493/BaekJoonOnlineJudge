e34d065ad67d86881e25288c9ff834da0df3a3a4
ywj3493 <37992140+ywj3493@users.noreply.github.com>
2019-01-03 15:10:47 +0900

    Add files via upload

---
+++ no11727.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int count[1001] = {0, };
+
+int cal(int n){
+    int& temp = count[n];
+    if(temp!=0) return temp;
+    return temp = (cal(n-1) + 2*cal(n-2))%10007;
+}
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    
+    count[1] = 1;
+    count[2] = 3;
+    
+    cal(n);
+    
+    printf("%d", count[n]);
+    
+    return 0;
+}
