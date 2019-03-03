cffa003812de3a6b16d258f5e2236f4b7c3b6884
ywj3493 <37992140+ywj3493@users.noreply.github.com>
2019-03-03 21:58:49 +0900

    토마토(3D)
    https://www.acmicpc.net/problem/7569

--- no7576.cpp
+++ no7576.cpp
@@ -22,13 +22,13 @@ bool check(){
 
 int main(){
     scanf("%d %d", &M, &N);
-    
+
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
             scanf("%d", &a[i][j]);
         }
     }
-    
+
     queue<pair<int, int>> q;
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
@@ -43,21 +43,19 @@ int main(){
         for(int i = 0; i < 4; i++){
             int nx = cx + dx[i];
             int ny = cy + dy[i];
-            if(nx>=0&&nx<N&&ny>=0&&ny<M&&(a[nx][ny]!=-1)){
-                if(a[nx][ny]==0){
-                    q.push(make_pair(nx,ny));
-                    a[nx][ny] = a[cx][cy] + 1;
-                    
-                }
+            if(nx>=0&&nx<N&&ny>=0&&ny<M&&(a[nx][ny]==0)){
+                q.push(make_pair(nx,ny));
+                a[nx][ny] = a[cx][cy] + 1;
+
             }
         }
         q.pop();
     }
-    
+
     if(check()){
         printf("%d\n", mx-1);
     }else printf("%d", -1);
-    
-    
+
+
     return 0;
-}
+} 
